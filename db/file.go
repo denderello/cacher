cdbc6f40cc55f6f1a114e66e044d1b8a31a6d8b2
Dennis Benkert <spinecrasher@googlemail.com>
2015-04-22 23:17:45 +0200

    added naive slow database implementation

---
+++ db/file.go
@@ -0,0 +1,68 @@
+package db
+
+import (
+	"encoding/csv"
+	"os"
+
+	"github.com/juju/errgo"
+)
+
+type SlowFileDatabase struct {
+	file      *os.File
+	state     map[string]string
+	csvReader *csv.Reader
+	csvWriter *csv.Writer
+}
+
+func NewSlowFileDatabase(filename string) (*SlowFileDatabase, error) {
+	f, err := os.OpenFile(filename, os.O_RDWR, 0666)
+	if err != nil {
+		return nil, errgo.Mask(err)
+	}
+
+	return &SlowFileDatabase{
+		file:      f,
+		csvReader: csv.NewReader(f),
+		csvWriter: csv.NewWriter(f),
+	}, nil
+}
+
+func (sfb *SlowFileDatabase) Open() error {
+	rs, err := sfb.csvReader.ReadAll()
+	if err != nil {
+		return errgo.Mask(err)
+	}
+
+	sfb.state = make(map[string]string)
+	for _, r := range rs {
+		sfb.state[r[0]] = r[1]
+	}
+
+	return nil
+}
+
+func (sfb *SlowFileDatabase) Close() {
+	sfb.file.Close()
+}
+
+func (sfb *SlowFileDatabase) Get(key string) (string, error) {
+	if v, ok := sfb.state[key]; ok {
+		return v, nil
+	}
+	return "", errgo.Newf("Could not find value for key '%s'", key)
+}
+
+func (sfb *SlowFileDatabase) Set(key, value string) error {
+	sfb.state[key] = value
+
+	return errgo.Mask(sfb.synchronizeToDisk())
+}
+
+func (sfb *SlowFileDatabase) synchronizeToDisk() error {
+	var rs [][]string
+	for k, v := range sfb.state {
+		rs = append(rs, []string{k, v})
+	}
+
+	return errgo.Mask(sfb.csvWriter.WriteAll(rs))
+}
--- main.go
+++ main.go
@@ -1,17 +1,31 @@
 package main
 
 import (
+	"log"
+
+	"github.com/denderello/cacher/db"
 	"github.com/denderello/cacher/server"
 	"github.com/spf13/pflag"
 )
 
 var (
-	serverAddr = pflag.String("server-addr", "0.0.0.0", "Address the server listens on")
-	serverPort = pflag.String("server-port", "8080", "Port the server listens on")
+	serverAddr   = pflag.String("server-addr", "0.0.0.0", "Address the server listens on")
+	serverPort   = pflag.String("server-port", "8080", "Port the server listens on")
+	databaseFile = pflag.String("database-file", "db.csv", "Databse file to use")
 )
 
 func main() {
 	pflag.Parse()
+
+	db, err := db.NewSlowFileDatabase(*databaseFile)
+	if err != nil {
+		log.Fatalf("Could not read databse file: %v", err)
+	}
+
+	if err := db.Open(); err != nil {
+		log.Fatalf("Could not open database file: %v", err)
+	}
+	defer db.Close()
 
 	s := server.NewHttpServer()
 	s.Start(*serverAddr, *serverPort)
