888932bb708392bb95628f34c1ca7251cb9e46be
Dennis Benkert <spinecrasher@googlemail.com>
2015-04-27 22:11:07 +0200

    added key cap for write stress tests

--- stress/request_counter.lua
+++ stress/request_counter.lua
@@ -3,6 +3,10 @@ counter = 0
 wrk.method = "POST"
 
 request = function()
+  if counter > 9000 then
+    counter = 0
+  end
+
   path = "/foo" .. counter .. "/bar" .. counter
   counter = counter + 1
   return wrk.format(nil, path)
